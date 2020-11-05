6ed26bb0a161eb71824c5e2a42c2bd08077b733b
zxum <zion.um@outlook.com>
2020-11-05 13:43:11 -0600

    Add basic authentication features

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,21 @@
+class UserController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Welcome #{@user.username}! Thank you for signing up!"
+    else 
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'user#new', as: 'signup'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/login', to: 'sessions#new', as: 'login'
+
+  resources :users 
+  resources :sessions
 end
