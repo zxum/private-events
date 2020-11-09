e12af2e1bb9110be1bee95f5849085bb9b96968d
zxum <zion.um@outlook.com>
2020-11-09 10:31:39 -0600

    Fix login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   def new 
-
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    user = User.find_by_username(params[:sessions][:username])
+    p params
     if user 
       session[:user_id] = user.id 
       redirect_to root_path, notice: "Logged in!"
@@ -18,4 +18,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private 
 
   def user_params 
-    params.require(:user).permit(:username)
+    params.require(:user).permit(:username, :id)
   end
 end
