affb4a8e87ca050a680b78043b37d0dfdf457d07
zxum <zion.um@outlook.com>
2020-11-10 10:47:18 -0600

    Fix authorization for events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
-
-
-  private 
+  before_action :signed_in_user
 
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     helper_method :current_user
+
+    def signed_in_user
+      unless user_signed_in?
+       redirect_to login_url, alert: "Not authorized"
+      end
+    end
+
+    def user_signed_in?
+      !current_user.nil?
+    end
+
+    helper_method :user_signed_in?
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  skip_before_action :signed_in_user, only: [:index]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
