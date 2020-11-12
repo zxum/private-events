bfb98af697cd18d81602e8eda413dc74b59b0fa4
zxum <zion.um@outlook.com>
2020-11-12 10:37:29 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create( [
+  { username: 'burgermeister' },
+  { username: 'tacoman' },
+  { username: 'muffinman' },
+  { username: 'chefpizza' }
+])
+
+users.first.events.create( [
+  { title: "Burger's Galore", location: 'At the Burger Bay', date: '2020-11-20 17:00:00', description: 'Dip into a simmering, hot, and delicious time with fresh burgers at the Burger Bay. This will be the event of a lifetime, with the debut of our newest addition, the Holy Hot Tamale Burger. Entrance is $10. No outside drinks or food allowed.'}
+])
+
+users[2].events.create([
+  { title: "Taco Tuesday", location: 'At the Taco Joint', date: '2020-11-10 15:00:00', description: "Bring your friends and family to Tacoman's Taco Tuesday event! There will be tacos, guac, and as much tortilla as you can fit in your bellies."}
+])
+
+users[3].events.create([
+  { title: "Cupcake Canoodling Party", location: 'At the Muffin Mansion', date: '2020-11-14 16:00:00', description: "Spend your Saturday afternoon at the Muffin Mansion and eat your heart's content of all the cupcakes we offer. Admission is $10 and you get a cupcake of your choice free with your entrance ticket."}
+])
+
+users[4].events.create([
+  { title: "Pizzeria Palooza IV", location: 'At the Pizzeria', date: '2022-11-27 17:00:00', description: "Come by the Pizzeria and compete in the fourth year of Pizzeria Palooza. Bystanders, fanclubs, and pizza enthusiasts are also welcome to watch this mouth-watering and blood-pumping feud of saucy dough and fresh toppings. The winning pizza shall be added to our menu for the whole year, until the next Pizzeria Palooza. Who will make the best pizza this year? It's up to you to decide!"}
+])
